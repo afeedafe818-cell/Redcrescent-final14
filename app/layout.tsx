a530c6bac5e2f8e8591848948aeef6b122c9cfdd
Afeedafe <afeedafe818@gmail.com>
2026-09-19 23:34:47 +0530

    Add website logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,15 @@ import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import "./globals.css";
 
+export const metadata = {
+  title: "Red Crescent Institute of Paramedical Sciences",
+  icons: {
+    icon: [{ url: "/Images/redcresent.jpg", type: "image/jpeg" }],
+    shortcut: "/Images/redcresent.jpg",
+    apple: "/Images/redcresent.jpg",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
